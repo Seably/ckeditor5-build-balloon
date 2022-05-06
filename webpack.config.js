4e474f86c8244c1266d3cfc2c98bd7e483a51127
David Svensson <david.svensson@orosberg.com>
2022-05-06 19:27:42 +0200

    Added new icons

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@
 
 const path = require( 'path' );
 const webpack = require( 'webpack' );
-const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
+const { styles } = require( '@ckeditor/ckeditor5-dev-utils' );
 const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
 const TerserPlugin = require( 'terser-webpack-plugin' );
 
@@ -50,10 +50,22 @@ module.exports = {
 			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
 			language: 'en'
 		} ),
-		new webpack.BannerPlugin( {
-			banner: bundler.getLicenseBanner(),
-			raw: true
-		} )
+		new webpack.NormalModuleReplacementPlugin(
+			/bold\.svg/,
+			path.resolve( __dirname, './src/theme/icons/bold.svg' )
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/italic\.svg/,
+			path.resolve( __dirname, './src/theme/icons/italic.svg' )
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/link\.svg/,
+			path.resolve( __dirname, './src/theme/icons/link.svg' )
+		),
+		new webpack.NormalModuleReplacementPlugin(
+			/bulletedlist\.svg/,
+			path.resolve( __dirname, './src/theme/icons/bulletedlist.svg' )
+		)
 	],
 
 	module: {
